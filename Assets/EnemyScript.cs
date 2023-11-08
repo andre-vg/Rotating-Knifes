fa6f063a91fcf6d646800980948069fd3d9bb030
andre-vg <avictorg@gmail.com>
2023-11-07 23:44:59 -0300

    Enemy and sword

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [Range(0f, 10f)]
+    public float enemySpeed = 1f;
+    public GameObject player;
+
+    //Function to start follow player
+
+    public void Update()
+    {
+        //Get player position
+        Vector3 playerPos = player.transform.position;
+
+        //Move enemy to player position
+        transform.position = Vector3.MoveTowards(transform.position, playerPos, Time.deltaTime * enemySpeed);
+    }
+}
